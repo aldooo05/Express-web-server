131d10708a19ac1604a29780776ab4b6e71b2f9e
Mardotilah Darmawan <mardotilahdarmawan29@gmail.com>
2023-11-09 00:50:46 +0700

    membuat permintaan GET,POST,PUT,DELETE

--- app.js
+++ app.js
@@ -1,33 +1,48 @@
+// Mengimpor modul Express.js
 const express = require("express");
+// Membuat aplikasi Express
 const app = express();
+// Menetapkan port server
 const port = 3000;
 
+// Menggunakan middleware untuk membaca data dari body permintaan dalam format JSON dan formulir yang dikodekan
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Menghandle permintaan GET ke path "/"
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  // Mengirimkan respons dengan pesan ke client
+  res.send("Hallo selamat datang di halaman utama");
 });
 
+// Menghandle permintaan GET ke path "/holla"
 app.get("/holla", (req, res) => {
-    console.log({ urlParam: req.query})
-  res.send("Hello Duniaaaa");
+  // Mencetak parameter URL yang diterima dari permintaan ke konsol
+  console.log({ urlParam: req.query });
+  // Mengirimkan respons dengan pesan ke client
+  res.send("Hello Ini halaman holla");
 });
 
+// Menghandle permintaan POST ke path "/login"
 app.post("/login", (req, res) => {
+  // Mencetak data dari body permintaan ke konsol
   console.log({ requestFromOutside: req.body });
-  const username = req.body.username
-//   if (username === usernameFromDbExist) {
-//     res.status(400).send("username tidak dapat digunakan")
-//   }
+  // Mengambil nilai dari properti "username" dalam body permintaan
+  const username = req.body.username;
+  // Mengirimkan respons ke client
   res.send("Masuk pa haji");
 });
 
+// Menghandle permintaan PUT ke path "/username"
 app.put("/username", (req, res) => {
+  // Mencetak data dari body permintaan ke konsol
   console.log({ updateData: req.body });
+  // Mengirimkan respons ke client
   res.send("Update data berhasil");
 });
 
+// Mengaktifkan server untuk mendengarkan permintaan pada port yang telah ditetapkan
 app.listen(port, () => {
+  // Mencetak pesan ke konsol ketika server telah dimulai
   console.log(`Example app listening on port ${port}`);
 });
